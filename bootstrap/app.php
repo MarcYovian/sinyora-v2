84d3ad78cdd782085e21f2932b18c7cc4d8fe5a3
marcellinus Yovian Indrastata <marcellinusyovian@gmail.com>
2025-05-07 12:19:29 +0700

    feat: Restore trustProxies middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
-        // $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
